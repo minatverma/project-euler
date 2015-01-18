fb23b985d7a7d29006cd9d5968adb80895134d24
Minat Kumar Verma <minat.verma@gmail.com>
2015-01-18 18:42:41 +0530

    modified
    ADD

---
+++ prime_of_rank.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10001st prime number?
+
+import os
+import sys
+import math
+from time import time
+
+
+def prime_of_rank(rank) :
+	current = 0
+	number = 2
+	start = time()
+	while current < rank :
+		if check_prime(number):
+			current = current + 1
+		if current == rank :
+			break
+		number = number + 1
+	print number
+	print "time taken in seconds", time() - start 
+
+def check_prime(number) :
+	if number < 2:
+		print "Not applicable to this number"
+	if number == 2 or number == 3:
+		return True
+	if number > 3 :
+		for i in range (2 ,number,1):
+			if number % i == 0:
+				return False
+	return True
+
+
+if __name__ == "__main__":
+	print "\nThis utility helps in finding the prime number of user input Rank"
+	try:
+		session = True
+		while session:
+			print "\n\nEnter the Rank of prime number you want to see . Enter 0 to exit ."
+			rank = raw_input()
+			if rank.isdigit():
+				if int(rank) == 0:
+					session = False
+				else:
+					prime_of_rank(int(rank))
+			else:
+				print "Please enter a valid number . Enter 0 to exit "
+	except  :
+		e = sys.exc_info()
+		print e
+		
+		
